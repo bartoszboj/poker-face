8dedd1612e61f6c94263299c8f4c7ab5b7ae422b
Bartosz Bojanowski <b.bojanowski@satisfly.co>
2024-09-23 14:18:09 +0200

    PF-6: toastify styles added

--- plugins/toastify.ts
+++ plugins/toastify.ts
@@ -1,4 +1,5 @@
 import Vue3Toastify, { type ToastContainerOptions, toast } from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
 export default defineNuxtPlugin((app) => {
     app.vueApp.use(Vue3Toastify, {
